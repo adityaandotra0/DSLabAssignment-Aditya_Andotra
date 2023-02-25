ae0a00a1c73b2121e209231dff4bb46744a4b5e4
Aditya Andotra <98409200+adityaandotra0@users.noreply.github.com>
2023-02-25 05:48:47 +0000

    Binary Tree

---
+++ Binary_Tree.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* create_node(int data) {
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+
+struct node* insert(struct node* root, int data) {
+    if (root == NULL) {
+        return create_node(data);
+    } else if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+void inorder(struct node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void preorder(struct node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct node* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main() {
+    struct node* root = NULL;
+    root = insert(root, 5);
+    root = insert(root, 3);
+    root = insert(root, 7);
+    root = insert(root, 1);
+    root = insert(root, 9);
+
+    printf("Inorder Traversal: ");
+    inorder(root);
+    printf("\n");
+
+    printf("Preorder Traversal: ");
+    preorder(root);
+    printf("\n");
+
+    printf("Postorder Traversal: ");
+    postorder(root);
+    printf("\n");
+
+    return 0;
+}
